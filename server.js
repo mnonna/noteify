7e026ee0efcd3c824859ee95fb36018b02cbc53d
mnonna <marcinnonna026@gmail.com>
2020-08-15 18:04:16 +0200

    node ssr fixed

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 var express = require("express");
-var serveStatic = require("serve-static");
+//var serveStatic = require("serve-static");
 var history = require("connect-history-api-fallback");
 var path = require("path");
 
 const app = express();
+
+const staticFileMiddleware = express.static(path.join(__dirname + "/dist"));
+
+app.use(staticFileMiddleware);
 app.use(
   history({
+    disableDotRule: true,
     verbose: true
   })
 );
 
-app.use(serveStatic(path.join(__dirname, "/dist")));
+app.use(staticFileMiddleware);
 
-var port = process.env.PORT || 8000;
-app.listen(port);
-console.log("server started " + port);
+app.get("/", function(req, res) {
+  res.render(path.join(__dirname + "/dist/index.html"));
+});
+
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
