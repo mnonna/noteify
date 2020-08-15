f203a07c3368445f5be12a311d07bde014da5887
mnonna <marcinnonna026@gmail.com>
2020-08-15 16:03:54 +0200

    Added routes history mode to node ssr

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 var express = require("express");
 var serveStatic = require("serve-static");
+var history = require("connect-history-api-fallback");
+var path = require("path");
+
 const app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(
+  history({
+    verbose: true
+  })
+);
+
+app.use(serveStatic(path.join(__dirname, "/dist")));
+
 var port = process.env.PORT || 8000;
 app.listen(port);
 console.log("server started " + port);
